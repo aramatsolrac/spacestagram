6ab7a9e6abd8d105e2ee089b520c682be0b41ab5
aramatsolrac <tamaraaselau@gmail.com>
2022-01-07 10:52:01 -0700

    Add like function and responsive design

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ const spaceData = document.getElementById("spaceData");
 window.onload = async function getImages() {
     const source = await (await fetch(`https://api.nasa.gov/planetary/apod?api_key=OCbAbDXICtYgGu3b2fbgmqZFJ4eP1Vff6GgQzq38&count=10`)).json();
     console.log(source)
-    const data = source.map(function(detail) {
+    const data = source.map(function(d) {
         spaceData.innerHTML += `
-            <div>
-                <img id="pic" src="${detail.hdurl}" alt="NASA Picture Of The Day"/>
-                <p>${detail.title}</p>
-                <p>${detail.date}</p>
+            <div class="spaceData__container">
+                <img id="pic" src="${d.hdurl}" alt="NASA Picture Of The Day"/>
+                <div class="card_details">
+                    <div>
+                        <p>${d.title}</p>
+                        <p>Date: ${d.date}</p>
+                    </div>
+                    <button class="btn-like" onclick="clickLikeBtn(this)">
+                        <i class="fas fa-heart" id="icon-like"></i>
+                    </button>
+                <div>
             </div>
             `
     })
 };
+
+function clickLikeBtn(like) {
+    like.classList.toggle("btn-liked");
+}
